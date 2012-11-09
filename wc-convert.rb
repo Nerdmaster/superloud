24c19c29285792f8e16da9937e6618037570443f
Nerdmaster <brody76@gmail.com>
2012-11-08 17:31:01 -0800

    ADD A WEECHAT CONVERTER WHEEEEEEEE (CHAT)

---
+++ wc-convert.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# Converts weechat logs into Messages data
+
+require "./loudbot"
+require "./data/messages"
+
+lines = IO.readlines("log.txt")
+
+@messages = Louds::Data::Messages.instance
+@messages.load
+
+for line in lines
+  (time, user, text) = line.split(/\t/)
+  text.strip!
+
+  # Skip non-user messages
+  next if user !~ /[A-Za-z]/
+
+  # Remove special characters which designate IRC status
+  user.sub!(/^[@+]/, "")
+
+  # Ignore anything that isn't loud
+  loud = false
+  is_it_loud?(text) do |status, reason|
+    loud = status == :loud
+  end
+  next unless loud
+
+  # Ignore superloud's own messages
+  next if user =~ /SUPERLOUD/
+
+  # Ignore commands
+  next if text =~ /^!/
+
+  # Ignore messages containing bot's name
+  next if text =~ /SUPERLOUD/
+
+  @messages.add(text, user)
+end
+
+@messages.serialize
