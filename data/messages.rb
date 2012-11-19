8658cdaf4538abd6cc47defa71072e28ac2a907a
Nerdmaster <brody76@gmail.com>
2012-11-19 15:33:18 -0800

    MOVE MESSAGE CLASS TO ITS OWN FILE

--- data/messages.rb
+++ data/messages.rb
@@ -2,48 +2,10 @@
 
 require "singleton"
 require "yaml"
+require File.dirname(__FILE__) + '/message'
 
 module Louds
 module Data
-
-class Message
-  attr_reader :author, :views, :score, :text
-  attr_accessor :container
-
-  # Loads a message's attributes from a hash
-  def self.from_hash(hsh)
-    msg = Message.new(hsh[:text], hsh[:author], hsh[:score], hsh[:views])
-
-    return msg
-  end
-
-  def initialize(text, author, score = 1, views = 0)
-    @text = text
-    @author = author
-    @views = views
-    @score = score
-  end
-
-  def view!
-    @views += 1
-    @container.dirty!
-  end
-
-  def upvote!
-    @score += 1
-    @container.dirty!
-  end
-
-  def downvote!
-    @score -= 1
-    @container.dirty!
-  end
-
-  # Converts all important attributes to a hash of data, primarily to ease exporting
-  def to_hash
-    return { :author => @author, :views => @views, :score => @score, :text => @text }
-  end
-end
 
 class Messages
   attr_reader :last
