5d5652988c1cd5f5a981d5c6c235144203e207bd
Nerdmaster <brody76@gmail.com>
2012-12-14 15:34:19 -0800

    MOVE IGNORE LIST SETUP INTO A METHOD

--- loudbot.rb
+++ loudbot.rb
@@ -12,7 +12,12 @@ def init_data
   @rps_data ||= {}
   @rps_contestant = {}
 
-  # Ignore list
+  # Load ignore list
+  load_ignore_list
+end
+
+# Loads the list of users to ignore
+def load_ignore_list
   @ignore_regexes = []
   ignores = FileTest.exist?("ignores.txt") ? IO.readlines("ignores.txt") : []
   for ignore in ignores
