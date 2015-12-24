9d2a1c65bf7ad743b03f867ac34737c1b8abb48f
Nerdmaster <brody76@gmail.com>
2015-12-24 15:39:48 -0800

    MAKE WEIGHT LOGIC LESS STUPID, DUMMY

--- louds/data/messages/sqlite.rb
+++ louds/data/messages/sqlite.rb
@@ -86,7 +86,7 @@ class Messages::SQLite < Messages
   def retrieve_messages
     # First, pull messages from the DB, favoring items with fewer views.  Skip anything
     # with a score of -10 or lower.
-    results = @db.execute(%Q|
+    allresults = @db.execute(%Q|
       SELECT id AS uid, text, score, author, views
       FROM messages WHERE score > -1
       ORDER BY views
@@ -96,44 +96,33 @@ class Messages::SQLite < Messages
     # We don't really care about order - we use "ORDER BY views" simply to
     # ensure new items get into the list in the unlikely event that we actually
     # accumulate more than 10k messages
-    results.shuffle!
+    allresults.shuffle!
 
-    while results.length > MINIMUM_RETRIEVAL_SIZE
-      # Take an item off the front of the stack
-      message = results.shift
+    # Just return the full set of results if we didn't pull very many beyond
+    # the bare minimum
+    if allresults.length <= MINIMUM_RETRIEVAL_SIZE*2
+      return allresults
+    end
 
-      # Everything gets a 100% chance at first
-      weight = 1.00
+    results = []
+    if results.length < MINIMUM_RETRIEVAL_SIZE
+      # We only want to pull a few over MINIMUM_RETRIEVAL_SIZE items on
+      # average, so our base weight is double the ratio of minimum items to
+      # total count retrieved.  This should make it very unlikely to pull
+      # fewer than the minimum without having too many.
+      base_chance = 3 * (MINIMUM_RETRIEVAL_SIZE.to_f / allresults.count)
 
-      # Modify chance of keeping based on score.  This formula penalizes
-      # 0-point louds significantly:
-      #
-      # * 0:  -0.433
-      # * 1:  -0.097
-      # * 2:  +0.065
-      # * 3:  +0.159
-      # * 4:  +0.221
-      # * 5:  +0.265
-      # * 10: +0.370
-      # * 20: +0.433
-      # * 50: +0.474
-      weight += 0.50 - (2.0 / ((message["score"] + 2) ** 1.1))
+      for message in allresults
+        # Weight formula: the higher the view count, the higher score needs to be to get a fair shot.
+        weight = base_chance * ((1+message["score"].to_f) ** 0.75 / (message["views"]+1))
 
-      # Modify chance of keeping based on views to give a very high chance to
-      # items not viewed, and stiff penalties to items viewed many times
-      weight += (100.0 / ((9 + message["views"]) ** 2)) - 0.75
+        # Add extra skew in favor of zero-view items
+        weight *= 2 if message["views"] == 0
 
-      # As a special case, 0-view louds get a 200% base chance since their
-      # score/views are pretty meaningless
-      weight = 2.0 if message["views"] == 0
-
-      # We only want to pull MINIMUM_RETRIEVAL_SIZE items on average, so our final
-      # modifier multiplies weight by the actual ratio of items to keep
-      weight *= MINIMUM_RETRIEVAL_SIZE / results.count
-
-      # If we decide to keep it, push this message to the back of the stack
-      if rand < weight
-        results.push message
+        # If we decide to keep it, push this message to the back of the stack
+        if rand < weight
+          results.push message
+        end
       end
     end
 
